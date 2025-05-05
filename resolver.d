1b8ee0b8a02fcaa43bce8b96a229ab8a511d0935
GogUrsu <119163056+GogUrsu@users.noreply.github.com>
2025-05-05 12:58:20 +0200

    Add files via upload

---
+++ resolver.d
@@ -0,0 +1,59 @@
+src/code/app/resolver.o src/code/app/resolver.o: \
+ ../src/code/app/resolver.c \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/resolver.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/gpio.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips_hw.h
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/resolver.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/gpio.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips_hw.h:
---
+++ startHere.d
@@ -0,0 +1,127 @@
+src/startHere.o src/startHere.o: ../src/startHere.c \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/app.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/gic.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/can.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xcanps.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xcanps_hw.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/gpio.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/lib/monitoring.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/timer.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xttcps.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xttcps_hw.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/rotaryFdb.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/encoder.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/drivers/quenc.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_io.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/resolver.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips.h \
+ ../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips_hw.h \
+ C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/canapp.h
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/app.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/gic.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/can.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xcanps.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xcanps_hw.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/gpio.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/lib/monitoring.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/peripheral/timer.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xttcps.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xttcps_hw.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/rotaryFdb.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/encoder.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/hardware/drivers/quenc.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/resolver.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips.h:
+
+../../BrdTest01_bsp/ps7_cortexa9_0/include/xspips_hw.h:
+
+C:\Users\vojnovici\Documents\AIT-Projekte\RISE\Resolver\ResolverExpansion_RiseV2\Software\TestingSoftware\project_1\project_1.sdk\BrdTest01\src\code/app/canapp.h:
